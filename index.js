d68d7b863e0b57992936aa9883260d32b891a191
Fazri Isalm <fazriislam.work@gmail.com>
2022-05-05 22:30:04 +0600

    Create update-get APT

--- index.js
+++ index.js
@@ -16,22 +16,38 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@assignment.n8fri.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('app is connected');
-  // perform actions on the collection object
-  client.close();
-});
+async function run() {
+  await client.connect();
+  const productsCollection = client.db('warehouse').collection('products');
+
+
+  app.get('/products', async (req, res) => {
+    const cursor = productsCollection.find({});
+    const products = await cursor.toArray();
+    res.send(products);
+  });
+
+  // Update API
+  app.get('/update/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const product = await productsCollection.findOne(query);
+    res.send(product);
+    // console.log("the id is", id);
+  })
+
+
+}
 
 
 
 
-
+run().catch(console.dir);
 
 app.get('/', async (req, res) => {
-    res.send('App is running');
+  res.send('App is running');
 })
 
 app.listen(port, () => {
-    console.log("App is listening", port);
+  console.log("App is listening", port);
 })
